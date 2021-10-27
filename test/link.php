38971e637035e071e5500fa56e3c1e295aea4219
Zachary Moore <50252234+ZM009@users.noreply.github.com>
2021-10-27 18:11:22 -0400

    Create link.php
    Created for github function purposes

---
+++ test/link.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<!-- For testing purposes only! -->
+<html>
+  <body>
+    <form method="post" action="test.php">
+      <p>User:<input type="text" name="user"> </p>
+      <p>ID:<input type="text" name="id"> </p>
+    </form>
+  </body>
+</html>
