63d89880f00b72a2657cff755ef5ca2b14c7e917
Zachary Moore <50252234+ZM009@users.noreply.github.com>
2021-10-27 17:54:34 -0400

    Create test.php
    Created a test.php file to test repository functionality.

---
+++ test/test.php
@@ -0,0 +1,8 @@
+<?php
+  if($connection = @mysqli_connect('localhost', 'zmoore4', 'zmoore4', 'SUGreekLifeDB') {
+    echo "Connect Succces";
+  } else {
+    die('Connect Failed: ' . mysqli_error());
+  }
+     
+?>
