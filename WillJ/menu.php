f981f18295ebef7b55f6da0530e8a9ffb3c7f12b
WgocowboyS <50374920+WgocowboyS@users.noreply.github.com>
2021-11-07 11:23:16 -0500

    Create menu.php
    TEMP Menu

---
+++ WillJ/menu.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+
+        a:link{
+                color:red;
+        }
+
+        a:visited{
+                color:green;
+        }
+
+        .menu{
+        border-radius: 25px;
+        border:4px solid #ffcc66;
+        margin:auto;
+        background-color: #d4af37;
+        width: 40%;
+        }
+
+</style>
+</head>
+
+
+<body>
+<div class= "menu">
+<?php
+echo '<a href="https://lamp.salisbury.edu/~wjenkins2/home.php">Home</a> -
+<a href="/html/default.asp">HTML Tutorial</a> -
+<a href="/css/default.asp">CSS Tutorial</a> -
+<a href="/js/default.asp">JavaScript Tutorial</a> -
+<a href="default.asp">PHP Tutorial</a>';
+?>
+</div>
+</body>
+</html>
