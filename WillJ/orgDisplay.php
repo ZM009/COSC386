ece7d6643b387d82c85ab5a37de496e3685124d4
WgocowboyS <50374920+WgocowboyS@users.noreply.github.com>
2021-11-07 16:11:54 -0500

    Update orgDisplay.php
    Added the motto and event display.

--- WillJ/orgDisplay.php
+++ WillJ/orgDisplay.php
@@ -7,7 +7,7 @@
         float:left;
         width:26%;
         color:white;
-        height:auto;
+        height:300px;
 }
 
 
@@ -15,12 +15,20 @@
         color: white;
         background-color: #d4af37;
         float: left;
-        width: 70%;
-        height: auto;
+        width: 35%;
+        height: 300px;
         }
 
-.backColor{
-        background-color:black;
+.lastSide{
+        width:45%;
+        background-color: #d4af37;
+        float: left;
+        height: 300px;
+        color:white;
+}
+
+h2{
+        text-align:center;
 
         }
 
@@ -32,8 +40,14 @@
  <?php
 $var_value = $_GET['varname'];
 include 'menu.php';
-
+$connect = @mysqli_connect('localhost','wjenkins2','wjenkins2','SUGreekLifeDB');
+$query = "select * from Organizations where name ='$var_value'";
+#echo $query;
+$r = mysqli_query($connect,$query);
+$row = mysqli_fetch_array($r);
 echo "<h1>" . $var_value . "</h1>";
+
+echo"<h2>'". $row['motto']  ."' </h2>";
 ?>
 
 <div class="memberDisplay">
@@ -43,8 +57,8 @@ $connect = @mysqli_connect('localhost','wjenkins2','wjenkins2','SUGreekLifeDB');
 $query = "select * from Students where orgName = '$var_value' ";
 $r = mysqli_query($connect, $query);
 echo "<table style='width:100%;text-align:left;'>";
-echo "<th style='height:50px;'> Name </th>";
-echo "<th style='height:50px;'> Vlounteer Hours </th>";
+echo "<th style='height:40px;'> Name </th>";
+echo "<th style='height:40px;'> Volunteer Hours </th>";
 while($row = mysqli_fetch_array($r)){
         echo "<tr>";
         echo "<td>" . $row['name'] . "</td>";
@@ -55,7 +69,6 @@ echo "</table>";
 #echo $query;
 ?>
 </div>
-
 <div class="info" style="padding-left:4%;">
 <h3> Officers: </h3>
 <?php
@@ -65,12 +78,12 @@ $query2 = "select name, position, years_held from governing_body, Students where
 $r = mysqli_query($connect,$query2);
 #echo $r;
 if(mysqli_num_rows($r) == 0){#no officers
-echo "<h2> There are no officers for " . $var_value . ". </h2>";
+echo "<h2> No officers for " . $var_value . ". </h2>";
 }else{ 
 echo "<table style = 'width: 50%;text-align:left;'>";
-echo "<th style='height:50px;'> Name </th>";
-echo "<th style='height:50px;'> Position </th>";
-echo "<th style='height:50px;'> Years Held </th>";
+echo "<th style='height:40px;'> Name </th>";
+echo "<th style='height:40px;'> Position </th>";
+echo "<th style='height:40px;'> Years Held </th>";
 
 while($row = mysqli_fetch_array($r)){
         echo "<tr>";
@@ -80,6 +93,34 @@ while($row = mysqli_fetch_array($r)){
         echo "</tr>";
 }
 echo "</table>";
+}
+?>
+</div>
+
+<div class="info">
+<?php
+
+echo "<h3> Events </h3>";
+$query = "select eventName, eventDate, eventLocation from held_by where orgName = '$var_value'";
+$r = mysqli_query($connect, $query);
+#echo $query;
+if(mysqli_num_rows($r) == 0){
+        echo "<h2 There are no events for " . $var_value . ". </h2>";
+}else{
+        echo "<table style='text-align:left;'>";
+        echo "<th style='height:40px;'> Name </th>";
+        echo "<th style='height:40px;'> Date </th>";
+        echo "<th style='height:40px;'> Location </th>";
+
+while($row = mysqli_fetch_array($r)){
+                echo "<tr>";
+                echo "<td>" . $row['eventName'] . "</td>";
+                echo "<td>" . $row['eventDate'] . "</td>";
+                echo "<td>" . $row['eventLocation'] . "</td>";
+                echo "</tr>";
+}
+echo "</table>";
+
 }
 ?>
 </div>
