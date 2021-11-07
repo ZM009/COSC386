35d3d593cb5650c72f11ddd73876906320f796bd
Zachary Moore <50252234+ZM009@users.noreply.github.com>
2021-11-07 14:59:05 -0500

    Updated navbar.php
    Changed the color of the navigation bar to uniformly fit the rest of the webpage.

--- Zach/navbar.php
+++ Zach/navbar.php
@@ -2,58 +2,65 @@
 <html>
 <head>
    <style>
-	/* styles the navigation bar itself */
-	.topnav {
-	   position: -webkit-sticky; /* Safari */
-	   position: sticky;
-	   top: 0;
-	   background-color: #800;
-	   overflow: hidden;
-	}
+        body {
+           margin: 0px;
+        }
+        ul {
+           list-style-type: none;
+           margin: 0px;
+           padding: 0px;
+        }
+        /* styles the navigation bar itself */
+        .topnav {
+           position: -webkit-sticky; /* Safari */
+           position: sticky;
+           top: 0;
+           background-color: black;
+           overflow: hidden;
+        }
 
-	/* Styles links in navigation bar */
-	.topnav a {
-	   float: left;
-	   color: #F2F2F2;
-	   text-align: center;
-	   padding: 14px 16px;
-	   text-decoration: none;
-	   font-size: 17px;
-	}
+        /* Styles links in navigation bar */
+        .topnav a {
+           float: left;
+           color: #F2F2F2;
+           text-align: center;
+           padding: 14px 16px;
+           text-decoration: none;
+           font-size: 17px;
+        }
 
-	/* :hover allows the color of the links to change when the mouse
+        /* :hover allows the color of the links to change when the mouse
            hovers over them */
-	.topnav a:hover {
-	   background-color: #DDD;
-	   color: black;
-	}
+        .topnav a:hover {
+           background-color: white;
+           color: black;
+        }
 
-	.topnav a.active {
-	   background-color: #ffd700;
-	   color: #800;
-	}
+        .topnav a.active {
+           background-color: #ffd700;
+           color: #800;
+        }
 
    </style>
 </head>
 <body>
    <div class="topnav" id="nav">
      <ul>
-	<a class="active" href="home.php">Home</a>
-	<a href="index.php">Login</a>
-	<a href="reg.php">Register</a>
-	<a href="#contact">Contact</a>
-	<a style="float:right" href="#admin">Admin</a>
-	<a href="#about">About</a>
+        <a class="active" href="https://lamp.salisbury.edu/~wjenkins2/home.php">Home</a>
+        <a href="https://lamp.salisbury.edu/~zmoore4/index.php">Login</a>
+        <a href="https://lamp.salisbury.edu/~zmoore4/reg.php">Register</a>
+        <a href="https://lamp.salisbury.edu/~zmoore4/contact.php">Contact</a>
+        <a style="float:right" href="#admin">Admin</a>
+        <a href="#about">About</a>
      </ul>
    </div>
-
    <script>
-	$(document).ready(function() {
-		$('a').click(function() {
-			$('a.active').removeClass('active');
-			$(this).addClass('active');
-		});
-	});
+        $(document).ready(function() {
+                $('a').click(function() {
+                        $("a.active").removeClass("active");
+                        $(this).addClass('active');
+                });
+        });
    </script>
 </body>
 </html>
