095de9f2e409bf85daf0cc37f50c80d9f0a8cc16
Hyperion1232 <93351057+Hyperion1232@users.noreply.github.com>
2021-11-07 14:24:54 -0500

    Create menu.php

---
+++ Andrew/menu.php
@@ -0,0 +1,108 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+
+         body{
+        background-color: maroon;
+        color:  #d4af37;
+        }
+
+        h1{
+                text-align: center;
+        }
+
+        aside{
+                background-color: green;
+        }
+
+        ul{
+                font-weight: 900;
+        }
+
+        ul li{
+                color: white;
+                font-weight: 700;
+        }
+
+        .container{
+                display: inline-block;
+        }
+
+        .center{        
+        display: inline;
+        text-align:center;
+        margin:auto;
+        
+        }
+
+        .boxText{
+        color: black;
+        text-align:center;
+        background-color: #ffcc66;
+        width: 600px;
+        border: 10px solid #d4af37;
+        padding: 40px;
+        margin-top:60px;
+        margin-right: auto;
+        margin-left:auto;
+        }
+        .pSubBox{
+                text-align:left;
+
+                }
+
+        img {
+   display: block;
+   margin-left: auto;
+  margin-right: auto;
+        }
+
+        .formSide{
+                padding-top:20px;
+                padding-left:42%;
+        }
+
+
+        
+        a:link{
+                color:red;
+        }
+
+        a:visited{
+                color:purple;
+        }
+
+        .menu{
+        border-radius: 25px;
+        border:4px solid #ffcc66;
+        margin:auto;
+        background-color: #d4af37;
+        width: 30%;
+        text-align:center;
+        }
+
+</style>
+</head>
+
+
+<body>
+<?php
+include 'navbar.php';
+?>
+
+<!-- <div class= "menu">
+<?php
+echo '<a href="https://lamp.salisbury.edu/~wjenkins2/home.php">Home</a> -
+<a href="/html/default.asp">Login</a> -
+<a href="/css/default.asp">Philanthropies</a> -
+<a href="/js/default.asp">Organizations</a> -
+<a href="default.asp">Events</a>';
+?>
+</div>
+-->
+<h1> Salisbury University Pantheon </h1>
+<img  src ="https://cosida.com/common/controls/image_handler.aspx?thumb_id=0&image_path=/images/2019/2/21/SalisburyLogo.png" alt = "Salisbury University Sammy the Seagull" style="width:200px;height:200px;">
+
+</body>
+</html>
