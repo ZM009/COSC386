14edc912a563133b3660a31547809cb61042588f
WgocowboyS <50374920+WgocowboyS@users.noreply.github.com>
2021-11-07 12:23:19 -0500

    Update menu.php
    Updated menu.php

--- WillJ/menu.php
+++ WillJ/menu.php
@@ -3,6 +3,68 @@
 <head>
 <style>
 
+         body{
+        background-color: maroon;
+        color:  #d4af37;
+        }
+
+        h1{
+                text-align: center;
+        }
+
+        aside{
+                background-color: green;
+        }
+
+        ul{
+                font-weight: 900;
+        }
+
+        ul li{
+                color: white;
+                font-weight: 700;
+        }
+
+        .container{
+                display: inline-block;
+        }
+
+        .center{        
+        display: inline;
+        text-align:center;
+        margin:auto;
+        
+        }
+
+        .boxText{
+        color: black;
+        text-align:center;
+        background-color: #ffcc66;
+        width: 600px;
+        border: 10px solid #d4af37;
+        padding: 40px;
+        margin-top:60px;
+        margin-right: auto;
+        margin-left:auto;
+        }
+        .pSubBox{
+                text-align:left;
+
+                }
+
+        img {
+   display: block;
+   margin-left: auto;
+  margin-right: auto;
+        }
+
+        .formSide{
+                padding-top:20px;
+                padding-left:42%;
+        }
+
+
+        
         a:link{
                 color:red;
         }
@@ -17,6 +79,7 @@
         margin:auto;
         background-color: #d4af37;
         width: 40%;
+        text-align:center;
         }
 
 </style>
