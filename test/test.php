e3134d9a21e5fee5be66af9174c5990dfd0ccf6d
Zachary Moore <50252234+ZM009@users.noreply.github.com>
2021-11-08 02:46:04 -0500

    Delete test directory
     Not needed anymore

--- test/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-  #Made for testing purporses
-  if($connection = @mysqli_connect('localhost', 'zmoore4', 'zmoore4', 'SUGreekLifeDB') {
-    echo "Connect Succces";
-  } else {
-    die('Connect Failed: ' . mysqli_error());
-  }
-     
-?>
