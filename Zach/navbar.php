ceb6347e9858e8c55ce42b82489cc42be6939785
Zachary Moore <50252234+ZM009@users.noreply.github.com>
2021-11-08 03:46:53 -0500

    Update navbar.php and add comments
    Added the functionality of the logout button (TEMPORARY USAGE for demo tomorrow)

--- Zach/navbar.php
+++ Zach/navbar.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
+   <script type"text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
    <style>
         body {
            margin: 0px;
         }
         ul {
-           list-style-type: none;
+           list-style-type: none;  /* kept in case <li> is ever used for navbar; learned from w3schools */
            margin: 0px;
            padding: 0px;
         }
         /* styles the navigation bar itself */
         .topnav {
-           position: -webkit-sticky; /* Safari */
-           position: sticky;
+           position: -webkit-sticky; /* -webkit prefix is req'd by Safari/MacOS (learned prefix from w3schools) */
+           position: sticky;   /* allows navbar to stick to the top of the page, even when user is scrolling */
            top: 0;
            background-color: black;
            overflow: hidden;
@@ -44,23 +45,23 @@
    </style>
 </head>
 <body>
-   <div class="topnav" id="nav">
+   <div class="topnav">
      <ul>
         <a class="active" href="https://lamp.salisbury.edu/~wjenkins2/home.php">Home</a>
         <a href="https://lamp.salisbury.edu/~zmoore4/index.php">Login</a>
         <a href="https://lamp.salisbury.edu/~zmoore4/reg.php">Register</a>
         <a href="https://lamp.salisbury.edu/~zmoore4/contact.php">Contact</a>
+        <a style="float:right" href="https://lamp.salisbury.edu/~zmoore4/logout.php">Logout</a>
         <a style="float:right" href="https://lamp.salisbury.edu/~bbogaczyk1/new.php">Admin</a>
         <a href="#about">About</a>
      </ul>
    </div>
    <script>
-        $(document).ready(function() {
-                $('a').click(function() {
-                        $("a.active").removeClass("active");
-                        $(this).addClass('active');
-                });
-        });
+      // script below is used for removing the active class and reassigning it to the current link clicked on
+      $('ul a').click(function() {
+         $('ul a').removeClass('active');
+         $(this).addClass('active');
+      });
    </script>
 </body>
 </html>
