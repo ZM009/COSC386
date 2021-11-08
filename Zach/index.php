fe36942acb4bedec4189254ada18d1b98a8b2204
Zachary Moore <50252234+ZM009@users.noreply.github.com>
2021-11-08 02:44:33 -0500

    Added php for login and comments (check desc.)
    php is added; redirects to Will's homepage until we find out what exactly to do with the login.

--- Zach/index.php
+++ Zach/index.php
@@ -1,61 +1,77 @@
 <?php
+   session_start();  // this is needed for logging in users
+		    // will either create or resume a session
+   $method = getenv('REQUEST_METHOD'); // used to get the value of REQUEST_METHOD, could be POST, GET
+   require_once 'config.php';
    require 'navbar.php';
+
+   // checks if data was posted to form
+   if($method == 'POST') {
+	/* mysqli_real_escape_string is used to protect against SQL injections, as it escapes any special characters
+	   that are inputted into the form.
+	*/
+	$username = mysqli_real_escape_string($connection, $_POST['username']);
+	$password = mysqli_real_escape_string($connection, $_POST['password']);
+	  
+	// checks if the fields have some form of data entered
+	if(!empty($username) && !empty($password)) {
+		// read from db to compare the login credentials
+		$query = "select * from Users where username = '$username' limit 1";   // limits the number of records returned by the query
+		$r = mysqli_query($connection, $query);  // the result of the query is stored in r
+
+		if($r) {
+			// checks if r holds a successful query, and if there is any data returned by the query, (i.e. not an empty set)
+		   if($r && mysqli_num_rows($r) > 0) {
+			  // the following will only execute if the query returns something other than an empty set
+			$dat = mysqli_fetch_assoc($r);  // dat will store the return of the query in an assciative array
+			if($dat['password'] === $password) {
+				$_SESSION['status'] = $dat['status'];   // sets the SESSION variable status to the associative array dat's value
+				// if the login is successful, header() is used to redirect user back to home page for now
+				header("Location: https://lamp.salisbury.edu/~wjenkins2/home.php");
+				//die;
+			}
+		   }
+		}
+		echo "Please enter a valid username or password";
+	}
+   }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
    <head>
 	<meta charset="UTF-8">
-	<title>SU Pantheon</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<title>Login</title>
    <style>
-     a.button {
-	border: none;
-	color: white;
-	padding: 16px 32px;
-	text-align: center;
-	text-decoration: none;
-	display: inline-block;
-	font-size: 16px;
-	margin: 4px 2px;
-	transition-duration: 0.4s;
-	cursor: pointer;   }
- 
-     a.register {
-	background-color: white;
-	color: black;
-	border: 2px solid #800000;   }
-
-     a.register:hover {
-	background-color: #FFD700;
-	color: #800000;   }
-
      body {
-/*	text-align: center; */
-	font: 14px sans-serif;
+	background-color: maroon;
+	color: #d4af37;
      }
-     .wrapper {
-	width: 360px;
-	padding: 20px;
+     fieldset {
+	width: 320px;
+	padding: 10px;
+	margin-bottom 10px;
+     }
+
+     legend {
+	font-weight: bold;
+     }
+
+     label {
+	font-family: Arial, sans-serif;
      }
    </style>
    </head>
    <body>
-	<div class="wrapper">
-	   <h2>Sign In<h2>
-	   <!--<a href="reg.php" class="button register">Click Here to Register</a><br>-->
-	   <form method="post" action="index.php">
-	       <div class="form-group">
-	          <label>Username</label>
-		  <input type="text" name="username" class="form-control" required="required" placeholder="Username"/><br>
-	       </div>
-	       <div class="form-group">
-		  <label>Password</label>
-	          <input type="password" name="password" class="form-control" required="required" placeholder="Password"/><br>
-	       </div>
-	       <div class="form-group">
-	          <input type="submit" class="btn btn-primary" value="Login"/>
-	       </div>
-	   <form>
-	</div>
+	   <h1>Sign In</h1>
+	   <form method="post" action="index.php">. <!-- if the action is not on its own page, if an error occurs, it could be thrown on another page -->
+	   <fieldset><legend>User Information</legend>
+	       <label>Username
+	       <input type="text" name="username" required="required" placeholder="Username"/></label><br><br>
+	       <label>Password: 
+	       <input type="password" name="password" required="required" placeholder="Password"/></label><br><br>
+	   </fieldset>
+	       <input type="submit" value="Login"/>
+	   </form>
    </body>
 </html>
