44d5f21b8b84b2c438ce07b3ff4c0938dbe2b58e
Hyperion1232 <93351057+Hyperion1232@users.noreply.github.com>
2021-11-22 10:27:59 -0500

    Create about.php

---
+++ Andrew/about.php
@@ -0,0 +1,20 @@
+<html>
+  
+<body>
+
+<div style="text-align:center;">
+<?php include 'menu.php';
+
+?>
+
+<div class="boxText">
+<h3> About Us </h3>
+<div class="pSubBox">
+<p>This website assists any potential new members understand Greek Life on campus, as well as assisting existing members understand more about their organizations. </p>
+<p> You can search for members of an organization, events held, find out which organizations are approved by certain councils, as well as keep track of the philanthropies they support and the corporations they work with.</p>
+</div>
+</div>
+
+</body>
+
+</html>
