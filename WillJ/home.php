65c11af04f7bea68838c2cedbcb52bdad0772794
WgocowboyS <50374920+WgocowboyS@users.noreply.github.com>
2021-12-07 14:57:22 -0500

    Update home.php
    Updated to make home page look nicer and more appleaing, plus fixed the database problem encountered.

--- WillJ/home.php
+++ WillJ/home.php
@@ -3,6 +3,16 @@
 
 <head>
 <title> Greek Life Database </title>
+
+<style>
+
+        .label{
+        text-align: left;
+
+        }
+
+</style>
+
 </head>
 
 <body>
@@ -15,61 +25,50 @@ $s =0;
 
 ?>
 <br>
-</div>
-<div class="boxText">
-<h3> Description of Website </h3>
-<div class="pSubBox">
-<p>This section is dedicated to explaining our website. </p>
-<p> More details will go here as we further understand HTML,CSS, and PHP. Until then we are keeping this work very limited.</p>
-</div>
-</div>
 
-<div class="formSide">
-        <form  action="https://lamp.salisbury.edu/~adepace1/studentSearch.php" method="post">
+<!-- <div class="center"> --!>
 
-        <p style="margin:auto;"> Search for Members By Organization:</p><input type="text" name = "orgStudent" placeHolder="Enter an Organization Name">
-        <input type ="submit" value="Search">
-        </form>
-        <br>
-        <form action="https://lamp.salisbury.edu/~adepace1/eventSearch.php" method="post">
-         <p style="margin:auto;"> Search for Events By Organization:</p><input type = "text" name="eventOrg" placeholder = "Enter an Organization Name">
-        <input type="submit" value="Search">
-        </form>
-</div>
-
-<div class="center">
+<div style="border: 2px solid black;">
+<div style="float:left; margin-left:20%;">
 <h2> Lists Of Councils At Salisbury </h2>
 <form action="" method="POST">
 
+<div style="text-align:left;margin-left:30px;">
 <?php
 $connect = @mysqli_connect('localhost','wjenkins2','wjenkins2','SUGreekLifeDB');
-$query = "select name from Councils";
+$query = "select distinct name from Councils";
 $r = mysqli_query($connect,$query);
 while($row = mysqli_fetch_array($r)){
                 $a = $a +1;
-                echo "<input type='radio' name='councilSelect' id=" . $a . " value=" . $a . ">";
-                echo"<label for=" . $a . ">" . $row['name'] . "</label>";
+                echo "<input type='radio' name='councilSelect' id=" . $a . " value=" . $a  . ">";
+                echo"<label for=" . $a .">" . $row['name'] . "</label>";
                 echo"<br>";
 }
-echo"<input type='submit' value='Select Council'>";
+#echo "<div style='text-align:center;'>";
+        echo"<input type='submit' value='Select Council' style='margin-left:20%;'>";
+#echo "</div>;"
 $s = $_POST["councilSelect"];
 ?>
 </form>
+</div>
+</div>
 
+<div style="float:none;">
 <?php
 if($s==0){
         #echo"NOT SELECTED";
 }else{
-        $query= "select councilName from Organizations";
+        $query= "select distinct councilName from Organizations";
 
         $ra = mysqli_query($connect,$query);
-                for($i = 0; $i <= $a; $i++){
+                for($i = 1; $i <= $a; $i++){
                         $row = mysqli_fetch_array($ra);
-                        if($s == $i)
+                        if($s == $i){
                                 $answer = $row["councilName"];
+                                }
                         }
 
-        echo"<br>";
+        #echo"<br>";
         echo "<h2 style='color:white;'>You have selected " . $answer . "</h2>";
         echo "<h4 style='color:white;'> Organizations Apart of This Fraternity/Sorority </h4>";
 
@@ -89,6 +88,7 @@ if($s==0){
 }
 ?>
 </div>
-
+</div>
 </body>
 </html>
+                                             one;">
