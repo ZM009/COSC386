54095ffbd79a86bc1aaaec502fd03587188db8f4
Hyperion1232 <93351057+Hyperion1232@users.noreply.github.com>
2021-12-08 17:27:51 -0500

    Update about.php

--- Andrew/about.php
+++ Andrew/about.php
@@ -1,8 +1,9 @@
 <html>
-<head>
-<title> About Us </title>  
-</head>
   
+<head>
+<title> About Us </title>
+</head>
+
 <body>
 
 <div style="text-align:center;">
@@ -13,7 +14,8 @@
 <div class="boxText">
 <h3> About Us </h3>
 <div class="pSubBox">
-<p>This website assists any potential new members understand Greek Life on campus, as well as assisting existing members understand more about their organizations. </p>
+<p>Fraternities and sororities are value-based, undergraduate organizations that are mainly associated with colleges and universities. At their core, they provide academic support and help young college students develop social and leadership skills.</p>
+<p>This website assists any potential new members with understanding Greek Life on campus, as well as assisting existing members understand more about their organizations. </p>
 <p> You can search for members of an organization, events held, find out which organizations are approved by certain councils, as well as keep track of the philanthropies they support and the corporations they work with.</p>
 </div>
 </div>
